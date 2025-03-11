ba980205555772cb73f982b4a8bc06d9dbaf6d11
nguy4803 <nguy4803@umn.edu>
2025-03-11 18:45:19 -0500

    Basic website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app-container">
+      <div className="content">
+        <h1>Hi, I'm Theo</h1>
+        <p>I'm currently attending the University of Minnesota majoring in Computer Science and minoring in Math</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href="/Theodore_Nguyen_Resume.pdf" download className="resume-link">
+            Here's my resume
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
